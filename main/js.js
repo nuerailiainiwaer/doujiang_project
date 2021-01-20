84ac4f0de3613e0505c7db8047ac59eeeaeff3d5
nuerailiainiwaer <58279394+nuerailiainiwaer@users.noreply.github.com>
2021-01-19 21:39:09 -0500

    mian page is done

---
+++ main/js.js
@@ -0,0 +1,28 @@
+img_1 = () => {
+    document.getElementById("img_1").src = "../img/icon_men/1.png";
+    document.getElementById("img_2").src = "../img/icon_men/02.png";
+    document.getElementById("img_3").src = "../img/icon_men/03.png";
+    document.getElementById("img_4").src = "../img/icon_men/04.png";
+
+
+}
+img_2 = () => {
+    document.getElementById("img_1").src = "../img/icon_men/01.png";
+    document.getElementById("img_2").src = "../img/icon_men/2.png";
+    document.getElementById("img_3").src = "../img/icon_men/03.png";
+    document.getElementById("img_4").src = "../img/icon_men/04.png";
+
+}
+img_3 = () => {
+    document.getElementById("img_1").src = "../img/icon_men/01.png";
+    document.getElementById("img_2").src = "../img/icon_men/02.png";
+    document.getElementById("img_3").src = "../img/icon_men/3.png";
+    document.getElementById("img_4").src = "../img/icon_men/04.png";
+
+}
+img_4 = () => {
+    document.getElementById("img_1").src = "../img/icon_men/01.png";
+    document.getElementById("img_2").src = "../img/icon_men/02.png";
+    document.getElementById("img_3").src = "../img/icon_men/03.png";
+    document.getElementById("img_4").src = "../img/icon_men/4.png";
+}

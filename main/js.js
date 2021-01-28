f5b707205aa8c0411c5b7182466c470d04457357
nuerailiainiwaer <58279394+nuerailiainiwaer@users.noreply.github.com>
2021-01-27 23:43:37 -0500

    first page done

--- main/js.js
+++ main/js.js
@@ -1,28 +1,81 @@
-img_1 = () => {
-    document.getElementById("img_1").src = "../img/icon_men/1.png";
-    document.getElementById("img_2").src = "../img/icon_men/02.png";
-    document.getElementById("img_3").src = "../img/icon_men/03.png";
-    document.getElementById("img_4").src = "../img/icon_men/04.png";
+ //iniitional
+ $('body').scrollspy({
+     target: "#main-nav"
+ });
+ //smoth scrolling 
+
+ // Smooth Scrolling
+ $("#main-nav a").on('click', function(event) {
+     if (this.hash !== "") {
+         event.preventDefault();
+
+         const hash = this.hash;
+
+         $('html, body').animate({
+             scrollTop: $(hash).offset().top
+         }, 800, function() {
+
+             window.location.hash = hash;
+         });
+     }
+ });
 
 
-}
-img_2 = () => {
-    document.getElementById("img_1").src = "../img/icon_men/01.png";
-    document.getElementById("img_2").src = "../img/icon_men/2.png";
-    document.getElementById("img_3").src = "../img/icon_men/03.png";
-    document.getElementById("img_4").src = "../img/icon_men/04.png";
+ img1 = () => {
+     document.getElementById("img_1").src = "../img/icon_men/1.png";
+     document.getElementById("img_2").src = "../img/icon_men/02.png";
+     document.getElementById("img_3").src = "../img/icon_men/03.png";
+     document.getElementById("img_4").src = "../img/icon_men/04.png";
+     document.getElementById("firsttop").style.height = "12rem";
+     //  document.getElementById("secondttop").style.height = "0";
+     //  document.getElementById("thirdtop").style.height = "0";
+     //  document.getElementById("fourthtop").style.height = "0";
 
-}
-img_3 = () => {
-    document.getElementById("img_1").src = "../img/icon_men/01.png";
-    document.getElementById("img_2").src = "../img/icon_men/02.png";
-    document.getElementById("img_3").src = "../img/icon_men/3.png";
-    document.getElementById("img_4").src = "../img/icon_men/04.png";
 
-}
-img_4 = () => {
-    document.getElementById("img_1").src = "../img/icon_men/01.png";
-    document.getElementById("img_2").src = "../img/icon_men/02.png";
-    document.getElementById("img_3").src = "../img/icon_men/03.png";
-    document.getElementById("img_4").src = "../img/icon_men/4.png";
-}
+ }
+ img2 = () => {
+     document.getElementById("img_1").src = "../img/icon_men/01.png";
+     document.getElementById("img_2").src = "../img/icon_men/2.png";
+     document.getElementById("img_3").src = "../img/icon_men/03.png";
+     document.getElementById("img_4").src = "../img/icon_men/04.png";
+     //  document.getElementById("firsttop").style.height = "0";
+     document.getElementById("secondttop").style.height = "12rem";
+     //  document.getElementById("thirdtop").style.height = "0";
+     //  document.getElementById("fourthtop").style.height = "0";
+
+ }
+ img3 = () => {
+     document.getElementById("img_1").src = "../img/icon_men/01.png";
+     document.getElementById("img_2").src = "../img/icon_men/02.png";
+     document.getElementById("img_3").src = "../img/icon_men/3.png";
+     document.getElementById("img_4").src = "../img/icon_men/04.png";
+     //  document.getElementById("firsttop").style.height = "0";
+     //  document.getElementById("secondttop").style.height = "0";
+     document.getElementById("thirdtop").style.height = "12rem";
+     //  document.getElementById("fourthtop").style.height = "0";
+
+ }
+ img4 = () => {
+     document.getElementById("img_1").src = "../img/icon_men/01.png";
+     document.getElementById("img_2").src = "../img/icon_men/02.png";
+     document.getElementById("img_3").src = "../img/icon_men/03.png";
+     document.getElementById("img_4").src = "../img/icon_men/4.png";
+     //  document.getElementById("firsttop").style.height = "0";
+     //  document.getElementById("secondttop").style.height = "0";
+     //  document.getElementById("thirdtop").style.height = "0";
+     document.getElementById("fourthtop").style.height = "12rem";
+ }
+
+ normalImg = () => {
+     console.log("baba")
+     document.getElementById("firsttop").style.height = "0";
+     document.getElementById("secondttop").style.height = "0";
+     document.getElementById("thirdtop").style.height = "0";
+     document.getElementById("fourthtop").style.height = "0";
+
+     document.getElementById("firsttop").style.transition = "all .5s";
+     document.getElementById("secondttop").style.transition = "all .5s";
+     document.getElementById("thirdtop").style.transition = "all .5s";
+     document.getElementById("fourthtop").style.transition = "all .5s";
+
+ }
---
+++ second/js.js
@@ -0,0 +1,47 @@
+img_1 = () => {
+    // document.getElementById("img_1").src = "../img/icon_men/01.png";
+    // document.getElementById("img_2").src = "../img/icon_men/02.png";
+    document.getElementById("img_1").src = "./img/logo05.jpg";
+    document.getElementById("img_2").src = "./img/logo4.jpg";
+    document.getElementById("img_3").src = "./img/logo3.jpg";
+    document.getElementById("img_4").src = "./img/logo2.jpg";
+    document.getElementById("img_5").src = "./img/logo1.jpg";
+    // document.getElementById("img_4").src = "../img/icon_men/04.png";
+
+}
+img_2 = () => {
+    document.getElementById("img_1").src = "./img/logo5.jpg";
+    document.getElementById("img_2").src = "./img/logo04.jpg";
+    document.getElementById("img_3").src = "./img/logo3.jpg";
+    document.getElementById("img_4").src = "./img/logo2.jpg";
+    document.getElementById("img_5").src = "./img/logo1.jpg";
+
+
+}
+
+
+img_3 = () => {
+    document.getElementById("img_1").src = "./img/logo5.jpg";
+    document.getElementById("img_2").src = "./img/logo4.jpg";
+    document.getElementById("img_3").src = "./img/logo03.jpg";
+    document.getElementById("img_4").src = "./img/logo2.jpg";
+    document.getElementById("img_5").src = "./img/logo1.jpg";
+    // document.getElementById("img_4").src = "../img/icon_men/04.png";
+
+}
+img_4 = () => {
+    document.getElementById("img_1").src = "./img/logo5.jpg";
+    document.getElementById("img_2").src = "./img/logo4.jpg";
+    document.getElementById("img_3").src = "./img/logo3.jpg";
+    document.getElementById("img_4").src = "./img/logo02.jpg";
+    document.getElementById("img_5").src = "./img/logo1.jpg";
+
+}
+img_5 = () => {
+    document.getElementById("img_1").src = "./img/logo5.jpg";
+    document.getElementById("img_2").src = "./img/logo4.jpg";
+    document.getElementById("img_3").src = "./img/logo3.jpg";
+    document.getElementById("img_4").src = "./img/logo2.jpg";
+    document.getElementById("img_5").src = "./img/logo01.jpg";
+
+}
